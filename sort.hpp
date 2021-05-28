6d2d4a24a63248fd36f51894231b66d8b06d0951
alice-thaitea <athai026@ucr.edu>
2021-05-27 21:25:57 -0700

    Made sort function into one class

--- sort.hpp
+++ sort.hpp
@@ -7,8 +7,24 @@
 class Sort {
     public:
         Sort() {}
-        virtual ~Sort() = default;
-        virtual void reorder(Moviedatabase* movies, std::vector<std::vector<std::string>> recommendations) = 0;
+        ~Sort() {}
+        void reorder(Moviedatabase& movies, int column_num) {
+            std::vector<std::vector<std::string>> r = movies.get_recommendations();
+            int i;
+            int j;
+            for (i = 1; i < r.size(); ++i) {
+                j = i;
+                while (j > 0) {
+                    double curr = std::stod(r.at(j).at(column_num));
+                    double prev = std::stod(r.at(j - 1).at(column_num));
+                    if (curr > prev) {
+                        swap(r.at(j), r.at(j - 1));
+                    }
+                    --j;
+                }
+            }
+            movies.set_recommendations(r);
+        }
 };
 
 
