dc38cc72e3fb3b221ce0db34cf77956e8e4e0b59
alice-thaitea <athai026@ucr.edu>
2021-05-27 21:25:15 -0700

    Added destructors, moved #endif to end of file

--- search.hpp
+++ search.hpp
@@ -6,7 +6,7 @@
 class Search
 {
 public:
-    virtual ~Search() = default;
+    virtual ~Search() {}
 
     virtual bool search(const Moviedatabase* movie, int row) const = 0;
 };
@@ -16,6 +16,7 @@ class Search_Column: public Search
 protected:
     int column;
 public:
+    ~Search_Column() {}
     Search_Column(const Moviedatabase* movie, const std::string& keyword)
     {
         column = movie->get_column_by_keyword(keyword);
@@ -33,6 +34,7 @@ class Search_Contains: public Search_Column
 {
     std::string query;
 public:
+    ~Search_Contains() {}
     Search_Contains(const Moviedatabase* movie, const std::string& column, const std::string& query) : Search_Column(movie, column)
     {
         this->query = query;
@@ -48,5 +50,41 @@ public:
     }
 };
 
-#endif //__SEARCH_HPP__
 
+class Search_And: public Search
+{
+    Search* search_one = nullptr;
+    Search* search_two = nullptr;
+public:
+    ~Search_And() {}
+    Search_And(Search* search_one, Search* search_two)
+    {
+        this->search_one = search_one;
+        this->search_two = search_two;
+    }
+    
+    bool search(const Moviedatabase* movie, int row) const
+    {
+        return search_one->search(movie, row) && search_two->search(movie, row);
+    }
+};
+
+class Search_Or: public Search
+{
+    Search* search_one = nullptr;
+    Search* search_two = nullptr;
+public:
+    ~Search_Or() {}
+    Search_Or(Search* search_one, Search* search_two)
+    {
+        this->search_one = search_one;
+        this->search_two = search_two;
+    }
+
+    bool search(const Moviedatabase* movie, int row) const
+    {
+        return search_one->search(movie, row)|| search_two->search(movie, row);
+    }
+};
+
+#endif //__SEARCH_HPP__
