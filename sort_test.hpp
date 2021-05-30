a6d0640a84e6da1e5deec5ff795d04e43ef019e5
alice-thaitea <athai026@ucr.edu>
2021-05-29 23:29:55 -0700

    Added sort by rating test on animation movies

--- sort_test.hpp
+++ sort_test.hpp
@@ -134,4 +134,25 @@ TEST(SortFunction, SortByYear_drama) {
     EXPECT_EQ(out.str(), "Title: Titanic\nGenre(s): romantic,drama\nDirector(s): James Cameron\nStarring: Leonardo DiCaprio and Kate Winslet\nRating: 7.8\nRelease Year: 1997\n\n");
 }
 
+TEST(SortFunction, SortByRating_animation) {
+    Moviedatabase movie;
+    movie.set_column_keywords({"Title","Genre","Director","Actress","Rate","Year"});
+    movie.add_row({"Titanic","romantic,drama","James Cameron","Leonardo DiCaprio and Kate Winslet","7.8","1997"});
+    movie.add_row({"Raya and The Last Dragon","animation,action,adventure","Don Hall, Carlos López Estrada","Kelly Marie Tran and Awkwafina","7.4","2021"});
+    movie.add_row({"The Mummy","action,thriller","Alex Kurtzman","Tom Cruise","7","2017"});
+    movie.add_row({"Skyscraper","action,thriller","Rawson Marshall Thurber","Dwayne Johnson","6","2018"});
+    movie.add_row({"Deadpool 2","action,adventure,sci-fi","David Leitch","Ryan Reynolds","8","2018"});
+    movie.add_row({"Maleficent","fantasy,adventure","Robert Stromberg","Angelina Jolie","7","2014"});
+    movie.add_row({"The Help","romantic,historical","Tate Taylor","Emma Stone","8","2011"});
+    movie.add_row({"Insidious","horror,thriller","James Wan","Patrick Wilson and Rose Byrne","7","2010"});
+    movie.add_row({"Frozen","animation","Chris Buck and Jennifer Lee","Kristen Bell, Idina Menzel and Jonathan Groff","7","2013"});
+    movie.set_search(new Search_Contains(&movie,"Genre","animation"));
+    movie.save_recommendation();
+    Sort s;
+    s.reorder(movie, 4);
+    std::stringstream out;
+    movie.print_recommendation(out);
+    EXPECT_EQ(out.str(), "Title: Raya and The Last Dragon\nGenre(s): animation,action,adventure\nDirector(s): Don Hall, Carlos López Estrada\nStarring: Kelly Marie Tran and Awkwafina\nRating: 7.4\nRelease Year: 2021\n\nTitle: Frozen\nGenre(s): animation\nDirector(s): Chris Buck and Jennifer Lee\nStarring: Kristen Bell, Idina Menzel and Jonathan Groff\nRating: 7\nRelease Year: 2013\n\n");
+}
+
 #endif //__SORT_TEST_HPP__
