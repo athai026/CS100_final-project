23cc7047f455edb5aa71f844fa69c286c790d3b3
alice-thaitea <athai026@ucr.edu>
2021-05-29 23:32:05 -0700

    Added sort by year test on action movies

--- sort_test.hpp
+++ sort_test.hpp
@@ -197,4 +197,25 @@ TEST(SortFunction, SortByRating_action) {
     EXPECT_EQ(out.str(), "Title: Deadpool 2\nGenre(s): action,adventure,sci-fi\nDirector(s): David Leitch\nStarring: Ryan Reynolds\nRating: 8\nRelease Year: 2018\n\nTitle: Raya and The Last Dragon\nGenre(s): animation,action,adventure\nDirector(s): Don Hall, Carlos López Estrada\nStarring: Kelly Marie Tran and Awkwafina\nRating: 7.4\nRelease Year: 2021\n\nTitle: The Mummy\nGenre(s): action,thriller\nDirector(s): Alex Kurtzman\nStarring: Tom Cruise\nRating: 7\nRelease Year: 2017\n\nTitle: Skyscraper\nGenre(s): action,thriller\nDirector(s): Rawson Marshall Thurber\nStarring: Dwayne Johnson\nRating: 6\nRelease Year: 2018\n\n");
 }
 
+TEST(SortFunction, SortByYear_action) {
+    Moviedatabase movie;
+    movie.set_column_keywords({"Title","Genre","Director","Actress","Rate","Year"});
+    movie.add_row({"Titanic","romantic,drama","James Cameron","Leonardo DiCaprio and Kate Winslet","7.8","1997"});
+    movie.add_row({"Raya and The Last Dragon","animation,action,adventure","Don Hall, Carlos López Estrada","Kelly Marie Tran and Awkwafina","7.4","2021"});
+    movie.add_row({"The Mummy","action,thriller","Alex Kurtzman","Tom Cruise","7","2017"});
+    movie.add_row({"Skyscraper","action,thriller","Rawson Marshall Thurber","Dwayne Johnson","6","2018"});
+    movie.add_row({"Deadpool 2","action,adventure,sci-fi","David Leitch","Ryan Reynolds","8","2018"});
+    movie.add_row({"Maleficent","fantasy,adventure","Robert Stromberg","Angelina Jolie","7","2014"});
+    movie.add_row({"The Help","romantic,historical","Tate Taylor","Emma Stone","8","2011"});
+    movie.add_row({"Insidious","horror,thriller","James Wan","Patrick Wilson and Rose Byrne","7","2010"});
+    movie.add_row({"Frozen","animation","Chris Buck and Jennifer Lee","Kristen Bell, Idina Menzel and Jonathan Groff","7","2013"});
+    movie.set_search(new Search_Contains(&movie,"Genre","action"));
+    movie.save_recommendation();
+    Sort s;
+    s.reorder(movie, 5);
+    std::stringstream out;
+    movie.print_recommendation(out);
+    EXPECT_EQ(out.str(), "Title: Raya and The Last Dragon\nGenre(s): animation,action,adventure\nDirector(s): Don Hall, Carlos López Estrada\nStarring: Kelly Marie Tran and Awkwafina\nRating: 7.4\nRelease Year: 2021\n\nTitle: Skyscraper\nGenre(s): action,thriller\nDirector(s): Rawson Marshall Thurber\nStarring: Dwayne Johnson\nRating: 6\nRelease Year: 2018\n\nTitle: Deadpool 2\nGenre(s): action,adventure,sci-fi\nDirector(s): David Leitch\nStarring: Ryan Reynolds\nRating: 8\nRelease Year: 2018\n\nTitle: The Mummy\nGenre(s): action,thriller\nDirector(s): Alex Kurtzman\nStarring: Tom Cruise\nRating: 7\nRelease Year: 2017\n\n");
+}
+
 #endif //__SORT_TEST_HPP__
