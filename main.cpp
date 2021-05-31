0f9da83884af80cd9c1152205872308b350aad68
daimonspilker <dspil005@ucr.edu>
2021-05-30 22:49:32 -0700

    implementation of menu *added menu selections and main interface for user *modified moviedatabasde.hpp to remove a duplicate read_file *made small changes to improve readability *added options for if the user enters Yes or y or Y when responding to if they want the recommendation to be sorted

--- main.cpp
+++ main.cpp
@@ -8,20 +8,66 @@
 #include <iosfwd>
 #include <sstream>
 #include <iostream>
+
 using namespace std;
 
 void searching(Moviedatabase& movie);
 void sorting(Moviedatabase& movie);
 
-
 int main(int argc, char* argv[])
 {
+    char select;
+    bool loop = true;
     Moviedatabase movie;
     movie.set_column_keywords({"Title","Genre","Director","Actor","Rate","Year"});
-    movie.read_file();
-    searching(movie);
-    cout << "Thank you" << endl;
-    return 0;
+    movie.read_file();    
+    cout << "Welcome to our movie recommender!" << endl;
+
+    while(loop)
+    {
+        cout << endl << "Please read the menu below and choose an option" << endl;
+        cout << "__________________________________________________" << endl;
+        cout << endl << "S - search for a movie" << endl;
+        //cout << "V - view favorite movie list" << endl;
+        //cout << "R - remove a movie from favorite movie list" << endl;
+        cout << "Q - quit and exit program" << endl;
+        cout << endl << "Enter selection: ";
+        cin >> select;
+        cout << endl;
+
+        switch(select)
+        {
+            case 'S':
+    	    case 's': 
+            {
+	        searching(movie);
+            } break;
+            case 'Q':
+            case 'q': 
+            {
+	        cout << "Thank you! Goodbye :)" << endl;
+		loop = false;
+	        return 0;
+            } break;
+            default: cout << "Invalid selection, please try again" << endl;
+        }
+        
+        cout << "Would you like to make another selection? (Y for yes or any other key for no)" << endl;
+        char c;
+        cin >> c;
+
+        if(c == 'y' || c == 'Y') 
+	{ 
+	    continue; 
+	} 
+        else 
+        { 
+            loop = false; 
+	    cout << endl << endl << "Thank you! Goodbye :)";
+	}
+    } 
+
+    return 0; 
 }
 
 void sorting(Moviedatabase& movie)
@@ -31,15 +77,17 @@ void sorting(Moviedatabase& movie)
     string user_ans2;
     cout << "Would you like your recommendation to be sorted? (yes/no)" << endl;
     cin >> user_ans1;
+    cout << endl;
 
-    if (user_ans1 == "yes") {
+    if (user_ans1 == "yes" || user_ans1 == "y" || user_ans1 == "Y" || user_ans1 == "Yes") {
         cout << "How would you like it sorted? By rating or by release year?" << endl;
         cin >> user_ans2;
+        cout << endl;
 
-        if (user_ans2 == "rating") {
+        if (user_ans2 == "rating" || user_ans2 == "r") {
             s.reorder(movie, 4);
         }
-        else if (user_ans2 == "year") {
+        else if (user_ans2 == "year" || user_ans2 == "y") {
             s.reorder(movie, 5);
         }
         else {
@@ -47,7 +95,7 @@ void sorting(Moviedatabase& movie)
         }
     }
     else {
-        cout << "No sorting:" << endl;
+        cout << "Not sorted:" << endl << endl;
     }
 }
 
@@ -56,15 +104,16 @@ void searching(Moviedatabase& movie)
     string user_input1;
     string user_input2;
     string user_input3;
-    cout<<"Choose your search option:"<< endl;
+    cout<<"Choose your search option:"<< endl << endl;
     cout<<"1.Actor "<< endl;
     cout<<"2.Genre"<< endl;
-    cout<<"3.Genre&Genre "<< endl;
+    cout<<"3.Genre & Genre "<< endl;
     cout<<"4.Genre/Genre "<< endl;
-    cout<<"5.Genre&Actor "<< endl;
-    cout<<"6.Genre&Director"<< endl;
+    cout<<"5.Genre & Actor "<< endl;
+    cout<<"6.Genre & Director"<< endl;
     cin >> user_input1;
-
+    cout << endl;
+ 
     stringstream to_Int1(user_input1);
     int input1 =0;
     to_Int1>>input1;
@@ -73,6 +122,7 @@ void searching(Moviedatabase& movie)
             cout <<"Enter name of the actress :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             movie.set_search(new Search_Contains(&movie,"Actor",user_input2));
             movie.save_recommendation();
             sorting(movie);
@@ -82,6 +132,7 @@ void searching(Moviedatabase& movie)
             cout <<"Enter genre of movie :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             movie.set_search(new Search_Contains(&movie,"Genre",user_input2));
             movie.save_recommendation();
             sorting(movie);
@@ -91,9 +142,11 @@ void searching(Moviedatabase& movie)
             cout <<"Enter your first genre :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             cout <<"Enter your second genre :"<< endl;
             cin.ignore();
             getline(cin, user_input3);
+	    cout << endl;
             movie.set_search(new Search_And(new Search_Contains(&movie,"Genre",user_input2),new Search_Contains(&movie,"Genre",user_input3)));
             movie.save_recommendation();
             sorting(movie);
@@ -103,9 +156,11 @@ void searching(Moviedatabase& movie)
             cout <<"Enter your first genre :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             cout <<"Enter your second genre :"<< endl;
             cin.ignore();
             getline(cin, user_input3);
+	    cout << endl;
             movie.set_search(new Search_Or(new Search_Contains(&movie,"Genre",user_input2),new Search_Contains(&movie,"Genre",user_input3)));
             movie.save_recommendation();
             sorting(movie);
@@ -115,9 +170,11 @@ void searching(Moviedatabase& movie)
             cout <<"Enter your genre :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             cout <<"Enter your actor :"<< endl;
             cin.ignore();
             getline(cin, user_input3);
+	    cout << endl;
             movie.set_search(new Search_And(new Search_Contains(&movie,"Genre",user_input2),new Search_Contains(&movie,"Actor",user_input3)));
             movie.save_recommendation();
             sorting(movie);
@@ -127,9 +184,11 @@ void searching(Moviedatabase& movie)
             cout <<"Enter your genre :"<< endl;
             cin.ignore();
             getline(cin, user_input2);
+	    cout << endl;
             cout <<"Enter your director :"<< endl;
             cin.ignore();
             getline(cin, user_input3);
+	    cout << endl;
             movie.set_search(new Search_And(new Search_Contains(&movie,"Genre",user_input2),new Search_Contains(&movie,"Director",user_input3)));
             movie.save_recommendation();
             sorting(movie);
